3ced5e15abd6c28f54b1144136feafafd303224b
abenezerbekele <abenezer.bm@protonmail.com>
2019-04-08 16:36:34 +0300

    initial

---
+++ app/Http/Controllers/ShelfController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\shelf;
+use Illuminate\Http\Request;
+
+class ShelfController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\shelf  $shelf
+     * @return \Illuminate\Http\Response
+     */
+    public function show(shelf $shelf)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\shelf  $shelf
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(shelf $shelf)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\shelf  $shelf
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, shelf $shelf)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\shelf  $shelf
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(shelf $shelf)
+    {
+        //
+    }
+}
