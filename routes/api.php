cff00352ab2d36e924c382ec685cb7dc656122cf
Alazar Kassahun <alazar.kas@gmail.com>
2019-05-30 12:42:54 +0300

    CORS issue solved

--- routes/api.php
+++ routes/api.php
@@ -13,28 +13,34 @@ use Illuminate\Http\Request;
 |
 */
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('cors')->group(function (){
-    Route::get('/shelves', 'ShelfController@index');
-    Route::get('/shelves/{shelf}', 'ShelfController@show');
-    Route::put('/shelves/{shelf}', 'ShelfController@update');
-    Route::post('/shelves', 'ShelfController@store');
-    Route::delete('/shelves/{shelf}', 'ShelfController@destroy');
+// Route::middleware('cors')->group(function (){
 
-    Route::get('/books', 'BookController@index');
-    Route::get('/books/{book}', 'BookController@show');
-    Route::get('/search', 'BookController@search');
-    Route::put('/books/{book}', 'BookController@update');
-    Route::post('/books', 'BookController@store');
-    Route::delete('/books/{book}', 'BookController@destroy');
+// } );
+Route::get('/shelves', 'ShelfController@index');
+Route::get('/shelves/{shelf}', 'ShelfController@show');
+Route::put('/shelves/{shelf}', 'ShelfController@update');
+Route::post('/shelves', 'ShelfController@store');
+Route::delete('/shelves/{shelf}', 'ShelfController@destroy');
+
+Route::get('/books', 'BookController@index');
+Route::get('/books/{book}', 'BookController@show');
+Route::get('/search', 'BookController@search');
+Route::put('/books/{book}', 'BookController@update');
+Route::post('/books', 'BookController@store');
+Route::delete('/books/{book}', 'BookController@destroy');
 
 
-    Route::post('/register', 'Auth\RegisterController@register');
-    // Route::post('/login', 'SessionsController@store');
-    Route::post('login', 'Auth\LoginController@login');
-} );
+Route::post('/register', 'Auth\RegisterController@register');
+// Route::post('/login', 'SessionsController@store');
+Route::post('login', 'Auth\LoginController@login');
+
 
